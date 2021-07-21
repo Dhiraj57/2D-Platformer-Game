f260d644e23f1116c3b665b2dee3a900a7847b9f
Dhiraj57 <58966263+Dhiraj57@users.noreply.github.com>
2021-07-21 10:19:48 +0530

    Crouch and jump animation implementation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,64 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
 
+    public float Speed;
+
     public void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
 
+        PlayerMovementAnimation(horizontal, vertical);
+
+        // play crouch animation
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+        {
+            if(animator != null)
+            {
+                animator.SetBool("Crouch", true);
+            }
+        }
+        else
+        {
+            if(animator != null)
+            {
+                animator.SetBool("Crouch", false);
+            }
+        }
+        
+    }
+
+    private void PlayerMovementAnimation(float horizontal, float vertical)
+    {
+        if(animator != null)
+        {
+            // Setting float value of speed inside animator
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+            // Setting value of 'jump' boolean
+            if (vertical > 0)
+            {
+                animator.SetBool("Jump", true);
+            }
+            else
+            {
+                animator.SetBool("Jump", false);
+            }
+        }
+
+        // Change the direction of player
         Vector3 scale = transform.localScale;
 
-        if(speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if(speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
 
         transform.localScale = scale;
-
     }
+
 }
