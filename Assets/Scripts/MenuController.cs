fcc3ac6a5c6b35a9af8aeccb2df47f414026cae4
Dhiraj57 <58966263+Dhiraj57@users.noreply.github.com>
2021-07-30 08:11:03 +0530

    Lobby Screen

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Info()
+    {
+        SceneManager.LoadScene("Info");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Start");
+    }
+}
